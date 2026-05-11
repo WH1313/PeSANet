f85e054cb36b8faef7b630061efd0400a9ddb251
WH1313 <61619311+WH1313@users.noreply.github.com>
2026-05-11 20:43:19 +0800

    Add package initializers

---
+++ peasnet/data/__init__.py
@@ -0,0 +1,5 @@
+"""Data helpers for user-provided PeSANet sequences."""
+
+from peasnet.data.sequence_dataset import SequenceDataset, load_npy_sequences
+
+__all__ = ["SequenceDataset", "load_npy_sequences"]
